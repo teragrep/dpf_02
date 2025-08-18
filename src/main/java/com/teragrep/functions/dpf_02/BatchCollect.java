ec5bf55869f5922c835ba0b1c8eea5e7e17af397
StrongestNumber9 <16169054+StrongestNumber9@users.noreply.github.com>
2025-08-18 14:10:02 +0300

    Adds building fixes

--- src/main/java/com/teragrep/functions/dpf_02/BatchCollect.java
+++ src/main/java/com/teragrep/functions/dpf_02/BatchCollect.java
@@ -69,7 +69,7 @@ import java.util.*;
  *   + call()
  * }
  *
- * BatchCollect --> SortByClause : sort order
+ * BatchCollect --&gt; SortByClause : sort order
  *
  * note right of  BatchCollect
  * Responsibilities:
