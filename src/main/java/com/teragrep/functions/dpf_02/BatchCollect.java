f350c2cd224b66ef5427bf497522fb3a1dbd78ef
StrongestNumber9 <16169054+StrongestNumber9@users.noreply.github.com>
2025-08-18 12:45:18 +0300

    Readd responsibilities

--- src/main/java/com/teragrep/functions/dpf_02/BatchCollect.java
+++ src/main/java/com/teragrep/functions/dpf_02/BatchCollect.java
@@ -58,6 +58,9 @@ import java.util.*;
  * @class BatchCollect
  * @brief Handles the business logic for processing orders.
  *
+ * @responsibilities
+ * - Collects rows
+ *
  * @collaborators
  * - SortByClause
  *
