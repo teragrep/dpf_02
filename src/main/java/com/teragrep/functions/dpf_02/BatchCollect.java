c8da0c7c340633f37932dff2bef58b0534ad6b6f
StrongestNumber9 <16169054+StrongestNumber9@users.noreply.github.com>
2025-08-19 09:56:19 +0300

    Remove @code

--- src/main/java/com/teragrep/functions/dpf_02/BatchCollect.java
+++ src/main/java/com/teragrep/functions/dpf_02/BatchCollect.java
@@ -69,7 +69,7 @@ import java.util.*;
  *   + call()
  * }
  *
- * {@code BatchCollect --> SortByClause : sort order}
+ * BatchCollect --> SortByClause : sort order
  *
  * note right of  BatchCollect
  * Responsibilities:
